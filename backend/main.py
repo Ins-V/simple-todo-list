a21580c53e9de6c27968d00a7c9c01aa65511d9e
Vitalii Husarev <shagode@ukr.net>
2021-03-19 14:40:20 +0200

    The alembic package is installed. And created task model migrations.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
-from config.database import Base, engine
 from api import router
 
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(router)
